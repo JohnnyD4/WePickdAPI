0cd955b758693011a1acae46c213550219e9b2e1
John Davis <johnsdavis95@gmail.com>
2018-03-25 15:50:44 -0400

    testing scraping images

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const request = require('request');
 const moment = require('moment');
+const scrapeIt = require("scrape-it")
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 const model = require('./model');
-const API = 'kmsmvb3ab2guzyrn9srj7xq7';
+const API = process.env.API_2;
+DATE = moment().format('YYYY-MM-DD');
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
@@ -22,22 +24,42 @@ app.post('/zip', (req, res) => {
     zipCode = `&zip=${zip}`;
   }
 
-  const url = `http://data.tmsapi.com/v1.1/movies/showings?startDate=2018-03-22${zipCode}&api_key=${API}`;
-  const list = request(url, (err, response, body)=> {
+  const url = `http://data.tmsapi.com/v1.1/movies/showings?startDate=${DATE}${zipCode}&api_key=${API}`;
+  const list = request(url, (err, response, body) => {
     if (err) throw err;
 
     const data = JSON.parse(body);
-    const build = [];
-    const obj = {};
+    const build = data.map(movie => ({ id: movie.rootId, title: movie.title, link: movie.showtimes[0].ticketURI }));
+// console.log(build);
+    const done = build.map(movie => {
+      // console.log(movie.link);
+      if (movie.link) {
+        scrapeIt(movie.link, {
+          avatar: {
+            selector: '.moviePoster img',
+            attr: 'src'
+          }
+        }, (err , { data }) => {
+          if (data.avatar) {
+            movie.image = data.avatar;
+          }
+          // console.log(movie);
+          return movie;
+        })
+        // .then(({ data, response }) => {
+        //   // console.log(data);
+        //   if (data.avatar) {
+        //     movie.image = data.avatar;
+        //   }
+        //   return movie;
+        // });
+      }
+      // console.log(movie);
+      return movie;
+    })
+    console.log(done);
+      res.send(done);
 
-    for (let i = 0; i < data.length; i += 1) {
-      const rootId = data[i].rootId;
-
-      obj[rootId] = data[i].title;
-      build.push(obj);
-    }
-
-    res.send(build);
   });
 });
 
@@ -62,10 +84,10 @@ app.get('/movie/:rootId', (req, res) => {
   // Get zip code from front end cookie/cache
   // 
   const zipCode = '&zip=33331';
-  const url = `http://data.tmsapi.com/v1.1/movies/showings?startDate=2018-03-22${zipCode}&api_key=${API}`;
+  const url = `http://data.tmsapi.com/v1.1/movies/showings?startDate=${DATE}${zipCode}&api_key=${API}`;
   const list = request(url, (err, response, body)=> {
     if (err) throw err;
-    
+
     const data = JSON.parse(body);
     const build = [];
 
@@ -95,7 +117,4 @@ app.post('/category', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
-  
-  // console.log(list);
-  // let results = JSON.parse(list.getBody('utf8'));
 });
